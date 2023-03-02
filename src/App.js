52951f94726516c8fddcb9f5a41659b94124ff9c
Rohanpatil22 <rohanpatil1797@gmail.com>
2023-03-02 22:55:32 +0530

    new component added and used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import pic from "./images/PWSkills-main.png";
 import pic_2 from "./images/search_icon.png";
+import main_img from "./images/main_img.jpg";
 import './App.css';
+import Card from "./Card";
+import card1 from "./images/card_1.jpg";
+import card2 from "./images/card_2.jpg";
+import card3 from "./images/card_3.jpg";
 
 function App() {
   return (
@@ -13,6 +18,25 @@ function App() {
       </div>
       <div><button id="login_btn">Login / Register</button></div>
     </div>
+   <div>
+      <ul class="flex justify-center gap-20 items-center mt-8 font-medium text-lg">
+        <li>Cources</li>
+        <li>PW Skills Lab</li>
+        <li>Job Portal</li>
+        <li>Experience Portal</li>
+        <li>Become an affiliate</li>
+        <li>Hall Of Fame</li>
+        <li>More</li>
+      </ul>
+    </div>
+   <div class="mt-8">
+    <img class="w-full"  src={main_img} alt="main_img"/>
+   </div>
+   <div class="flex justify-center gap-14">
+   <Card card_1={card1} heading="Full Stack Web Development" teacher="Hitesh Choudhary" />
+   <Card card_1={card2} heading="Java With DSA And System Design" teacher="Priya Bhatia"/>
+   <Card card_1={card3} heading="Data Science Masters" teacher="Krish Naik"/>
+   </div>
    
     </>
   );
---
+++ src/Card.jsx
@@ -0,0 +1,18 @@
+// import card_1 from "./images/card_1.jpg"
+
+
+function Card ({card_1, heading, teacher}) {
+return(
+
+    <>
+    <div class="w-1/4 border rounded-xl bg-slate-200 p-1 mb-10 mt-10">
+        <img class="w-full" src={card_1} alt="card_img_1"/>
+        <div class="mt-4 text-blue-800 text-xl font-bold">{heading}</div>
+        <div class="mt-10 font-normal text-lg">{teacher}</div>
+        <div class="mt-1 font-bold text-xl mb-8">₹2975.00</div>
+    </div>
+    </>
+);
+}
+
+export default Card;
